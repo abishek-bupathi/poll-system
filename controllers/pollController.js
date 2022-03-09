14fd3e5e56b473446c38752092b674fe39259955
Abishek Bupathi <abishekb128@gmail.com>
2022-03-09 18:52:57 +0000

    Added comments

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -5,9 +5,10 @@ var mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
+// Connecting to MongoDB
 mongoose.connect('mongodb+srv://abishekb128:mongoabi328@cluster0.g3nrx.mongodb.net/poll?retryWrites=true&w=majority');
 
-// Create a scheme - this is like a blueprint
+// Creating Poll schema to store language name and it's number of votes
 var pollSchema = new mongoose.Schema({
   name: String,
   no_of_votes: Number
@@ -27,32 +28,36 @@ module.exports = function (app) {
     });
   });
 
+  // Data from MongoDB sent to poll result page
   app.get('/poll_result', function (req, res) {
     // get data from mongodb and pass it to the view    
     Poll.find({}, function (err, data) {
       if (err) throw err;
       res.render('poll_result', { polls: data });
     });
-
   });
 
+  // When poll item vote needs to be updated, "poll/selected_lang" post request is recieved from frontend
   app.post('/poll/:selected_lang', urlencodedParser, function (req, res) {
+    // Printing the selected language on console for debug purpose
     console.log(req.params.selected_lang)
+    // Query with selected language name as the key.
     var selected_lang_query = { name: req.params.selected_lang }
 
+    // Finding the correspoding data using the query to get the exisiting no of votes for that language
     Poll.findOne(selected_lang_query, function (err, data) {
-      var updated_poll = {name: req.params.selected_lang, no_of_votes: (data.no_of_votes + 1)}
-
+      // Creating a new data with incremented no_of_votes field value
+      var updated_poll = { no_of_votes: (data.no_of_votes + 1) }
+      // Updating only the no_of_votes field for the corresponding document data with the selected language
       Poll.updateOne(selected_lang_query, updated_poll, function (err, data) {
         if (err) throw err;
         console.log("1 document updated");
         res.json(data)
-  
       });
-
     })
   });
 
+  // "/poll" post request is used when new data is to be created in the MongoDB
   app.post('/poll', urlencodedParser, function (req, res) {
     // get data from the view and add it to mongodb
     var new_polls = Poll(req.body).save(function (err, data) {
--- public/assets/poll.js
+++ public/assets/poll.js
@@ -1,49 +1,61 @@
 $(document).ready(function () {
 
+  // When submit button is clicked
   $('form').on('submit', function () {
 
+    // Get the selected language name
     var selected_lang = document.querySelector('input[name="language"]:checked').value;
+    // Get the list of all languages
     var languages = document.getElementsByName('language')
 
+    // Accessing the polls data send from res.render() through html element
     var polls_db = JSON.parse($('#polls_db').text());
     $('#polls_db').remove();
 
+    // If the polls data is empty i.e. First use
     if (polls_db.length == 0) {
 
       var new_polls_data = [];
 
+      // Loop runs through all the languages
       for (i = 0; i < languages.length; i++) {
+        // Initial value for votes set to 0
         var votes = 0;
+        // If this language is selected then votes is set to 1
         if (languages[i].value == selected_lang) {
           votes = 1;
         }
-        new_polls_data.push({name: languages[i].value, no_of_votes: votes})
+        // Adding the language name and it's votes to a array of data
+        new_polls_data.push({ name: languages[i].value, no_of_votes: votes })
       }
 
+      // Writing each language data into MongoDB using ajax post request
       for (i = 0; i < languages.length; i++) {
         $.ajax({
           type: 'POST',
           url: '/poll',
           data: new_polls_data[i],
           success: function (data) {
-            //do something with the data via front-end framework 
+            // Redirecting to the results page on successfull update
             window.location.href = "poll_result"
           }
         });
       }
 
     } else {
-      
+      // If polls data already exist
+      // The selected language name to be updated is sent through ajax post request
       $.ajax({
         type: 'POST',
         url: '/poll/' + selected_lang,
         success: function (data) {
-          //do something with the data via front-end framework 
+          // Redirecting to the results page on successfull update
           window.location.href = "poll_result"
         }
       });
     }
 
+    // Returning false to prevent browser's default behaviour
     return false;
   })
 
